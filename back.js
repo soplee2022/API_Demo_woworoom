e4bf9b213c692b887ee92cfc6ee1e060e54d49b4
Sophie Lee <2020soop@gmail.com>
2022-12-28 17:42:59 +0800

    [feat] render order list & delete btn

--- back.js
+++ back.js
@@ -6,15 +6,59 @@ const token = 'QAUmGOmqGMhc1e4clUy24HWNNmn1';
 let newData = [];
 
 // ---------- 後台：訂單功能 ----------
+const js_orderList = document.querySelector('.js_orderList');
+// 函式 => 設定訂單資訊
+const set_order_list = (item, index) => {
+  const { user } = item;
 
+  // 組時間字串
+  const turnTime = new Date(item.createdAt * 1000);
+  const orderTime = `${turnTime.getFullYear()}/${turnTime.getMonth() + 1}/${turnTime.getDate()}`;
+
+  // 組產品字串
+  let products_str = '';
+  item.products.map((item2) => {
+    products_str += `<p>${item2.title} * ${item2.quantity}</p>`;
+    return products_str;
+  });
+
+  str = `<tr>
+    <td>${item.id}</td>
+    <td>
+      <p>${user.name}</p>
+      <p>${user.tel}</p>
+    </td>
+    <td>${user.address}</td>
+    <td>${user.email}</td>
+    <td>
+      <p>${products_str}</p>
+    </td>
+    <td>${orderTime}</td>
+    <td class="orderStatus">
+      <button value="orderStatus" data-num="${index}">未處理</button>
+    </td>
+    <td>
+      <input type="button" class="delSingleOrder-Btn" value="刪除" data-num="${index}">
+    </td>
+  </tr>`;
+
+  return str;
+};
+// 函式 => 渲染產品列表
+const render_order_list = () => {
+  let str_all = '';
+  orders_ary.map((item, index) => {
+    str_all += set_order_list(item, index);
+    return str_all;
+  });
+  js_orderList.innerHTML = str_all;
+};
 // 函式 => axios 資料傳出來
 const call_data = () => {
   console.log(newData);
-  console.log(newData.products);
+  console.log(orders_ary);
 };
-
 // 函式 => 取得訂單列表
-// eslint-disable-next-line no-unused-vars
 const getOrderList = () => {
   axios
     .get(
@@ -27,21 +71,76 @@ const getOrderList = () => {
     )
     .then((res) => {
       newData = res.data;
-      product_ary = newData.products;
+      orders_ary = newData.orders;
       console.log(newData);
       call_data();
+      render_order_list();
     });
 };
 
-// 修改訂單狀態
+// 監聽 => 點擊修改訂單狀態、刪除訂單
+js_orderList.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { num } = e.target.dataset;
+  const { id } = orders_ary[num];
+  const statusBtn = e.target.value === 'orderStatus';
+  const deleteBtn = e.target.value === '刪除';
+
+  // 點擊修改訂單狀態
+  if (statusBtn) {
+    // sweet alert 確認是否修改訂單狀態
+    swal({
+      title: '請確認',
+      text: '點擊後將修改訂單狀態',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal('完成！訂單狀態已修改', {
+            icon: 'success',
+          });
+          // eslint-disable-next-line no-use-before-define
+          editOrderList(id);
+        } else {
+          swal('已保留原訂單狀態');
+        }
+      });
+  }
+  // 點擊刪除訂單
+  if (deleteBtn) {
+    // sweet alert 確認是否刪除訂單
+    swal({
+      title: '請確認',
+      text: '點擊後將刪除此筆訂單',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal('完成！訂單已刪除', {
+            icon: 'success',
+          });
+          // eslint-disable-next-line no-use-before-define
+          deleteOrderItem(id);
+        } else {
+          swal('已保留此筆訂單');
+        }
+      });
+  }
+});
+
+// (待修改：paid 狀態改參數)函式 => axios 修改訂單狀態
 // eslint-disable-next-line no-unused-vars
-function editOrderList(orderId) {
+const editOrderList = (id) => {
   axios
     .put(
       `https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders`,
       {
         data: {
-          id: orderId,
+          id,
           paid: true,
         },
       },
@@ -51,10 +150,11 @@ function editOrderList(orderId) {
         },
       },
     )
-    .then((response) => {
-      console.log(response.data);
+    .then((res) => {
+      console.log(res.data);
+      getOrderList();
     });
-}
+};
 
 // 刪除全部訂單
 // eslint-disable-next-line no-unused-vars
@@ -73,22 +173,23 @@ function deleteAllOrder() {
     });
 }
 
-// 刪除特定訂單
+// 函式 => axios 刪除特定訂單
 // eslint-disable-next-line no-unused-vars
-function deleteOrderItem(orderId) {
+const deleteOrderItem = (id) => {
   axios
     .delete(
-      `https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders/${orderId}`,
+      `https://livejs-api.hexschool.io/api/livejs/v1/admin/${api_path}/orders/${id}`,
       {
         headers: {
           Authorization: token,
         },
       },
     )
-    .then((response) => {
-      console.log(response.data);
+    .then((res) => {
+      console.log(res.data);
+      getOrderList();
     });
-}
+};
 
 // C3.js
 // const chart = c3.generate({
