393ac6303bab78db0291e7245e9278db1502769b
Sophie Lee <2020soop@gmail.com>
2022-12-30 13:55:34 +0800

    [feat] 後台引入 C3.js 圖表，點擊切換訂單狀態

--- back.js
+++ back.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable array-callback-return */
 /* eslint-disable no-undef */
 /* eslint-disable camelcase */
 const api_path = 'sop001';
@@ -24,6 +26,8 @@ const set_order_list = (item, index) => {
     return products_str;
   });
 
+  // 判斷訂單處理狀態
+  const orderStatus = item.paid === true ? '已處理' : '未處理';
   str = `<tr>
     <td>${item.id}</td>
     <td>
@@ -37,7 +41,7 @@ const set_order_list = (item, index) => {
     </td>
     <td>${orderTime}</td>
     <td class="orderStatus">
-      <button value="orderStatus" data-num="${index}">未處理</button>
+      <button value="orderStatus" data-num="${index}" data-status="${item.paid}">${orderStatus}</button>
     </td>
     <td>
       <input type="button" class="delSingleOrder-Btn" value="刪除" data-num="${index}">
@@ -79,6 +83,8 @@ const getOrderList = () => {
       call_data();
       // eslint-disable-next-line no-unused-expressions
       no_data ? js_orderList.innerHTML = no_orders : render_order_list();
+      chart1_productsAll();
+      chart2_productsAll();
     });
 };
 
@@ -223,45 +229,108 @@ const deleteOrderItem = (id) => {
     });
 };
 
-// C3.js
-// eslint-disable-next-line no-unused-vars
-const chart1 = c3.generate({
-  bindto: '#chart1', // HTML 元素綁定
-  data: {
-    type: 'pie',
-    columns: [
-      ['Louvre 雙人床架', 1],
-      ['Antony 雙人床架', 2],
-      ['Anty 雙人床架', 3],
-      ['其他', 4],
-    ],
-    colors: {
-      'Louvre 雙人床架': '#6a8eae',
-      'Antony 雙人床架': '#dddace',
-      'Anty 雙人床架': '#80ab82',
-      其他: '#423e3b',
-    },
-  },
-});
+// 整理 C3.js 資料
 
-// eslint-disable-next-line no-unused-vars
-const chart2 = c3.generate({
-  bindto: '#chart2', // HTML 元素綁定
-  data: {
-    type: 'pie',
-    columns: [
-      ['Louvre 雙人床架', 1],
-      ['Antony 雙人床架', 2],
-      ['Anty 雙人床架', 3],
-      ['其他', 4],
-    ],
-    colors: {
-      'Louvre 雙人床架': '#585123',
-      'Antony 雙人床架': '#aca691',
-      'Anty 雙人床架': '#eec170',
-      其他: '#d36135',
+// C3.js 的好朋友 aka 偉大的 Object.entries()
+// const test_obj = { x: 1, y: 2 };
+// console.log(Object.entries(test_obj));
+
+// Chart1：全產品營收比重
+const chart1_productsAll = () => {
+  call_data();
+  // 把物件資料整理成陣列資料 -> 取得產品品項 & 收入
+  const productObj = {};
+  let newChart1_ary = [];
+  orders_ary.map((item) => {
+    item.products.map((訂單品項) => {
+      if (productObj[訂單品項.title] === undefined) {
+        productObj[訂單品項.title] = 訂單品項.price * 訂單品項.quantity;
+      } else {
+        productObj[訂單品項.title] += 訂單品項.price * 訂單品項.quantity;
+      }
+    });
+    // console.log(productObj);
+    // 做資料關聯
+    newChart1_ary = Object.entries(productObj); // 實際擺進 C3.js 的陣列
+    // console.log(newChart_ary);
+  });
+
+  // 比大小 : 降冪排列
+  newChart1_ary.sort((a, b) => b[1] - a[1]);
+
+  // 營收前 3 名 + 其餘類別統整為其他
+  if (newChart1_ary.length > 3) {
+    let otherRevenue = 0;
+    // eslint-disable-next-line array-callback-return
+    newChart1_ary.map((item, index) => {
+      if (index > 2) {
+        otherRevenue += newChart1_ary[index][1];
+      }
+    });
+    newChart1_ary.splice(3, newChart1_ary.length - 1);
+    newChart1_ary.push(['其他', otherRevenue]);
+    // console.log(newChart1_ary);
+  }
+  // eslint-disable-next-line no-unused-vars
+  const chart1 = c3.generate({
+    bindto: '#chart1', // HTML 元素綁定
+    data: {
+      type: 'pie',
+      columns: newChart1_ary,
+      colors: {
+        pattern: ['#f4cf00', '#7bb001', '#07b6f5', '#f54ea9'],
+      },
     },
-  },
-});
+  });
+};
+
+// Chart2：全產品類別營收比重
+const chart2_productsAll = () => {
+  call_data();
+  // 把物件資料整理成陣列資料 -> 取得產品品項 & 收入
+  const productObj = {};
+  let newChart2_ary = [];
+  orders_ary.map((item) => {
+    item.products.map((訂單品項) => {
+      if (productObj[訂單品項.category] === undefined) {
+        productObj[訂單品項.category] = 訂單品項.price * 訂單品項.quantity;
+      } else {
+        productObj[訂單品項.category] += 訂單品項.price * 訂單品項.quantity;
+      }
+    });
+    // console.log(productObj);
+    // 做資料關聯
+    newChart2_ary = Object.entries(productObj); // 實際擺進 C3.js 的陣列
+    // console.log(newChart_ary);
+  });
+
+  // 比大小 : 降冪排列
+  newChart2_ary.sort((a, b) => b[1] - a[1]);
+
+  // 營收前 3 名 + 其餘類別統整為其他
+  if (newChart2_ary.length > 3) {
+    let otherRevenue = 0;
+    // eslint-disable-next-line array-callback-return
+    newChart2_ary.map((item, index) => {
+      if (index > 2) {
+        otherRevenue += newChart2_ary[index][1];
+      }
+    });
+    newChart2_ary.splice(3, newChart2_ary.length - 1);
+    newChart2_ary.push(['其他', otherRevenue]);
+    // console.log(newChart2_ary);
+  }
+  // eslint-disable-next-line no-shadow, no-unused-vars
+  const chart2 = c3.generate({
+    bindto: '#chart2', // HTML 元素綁定
+    data: {
+      type: 'pie',
+      columns: newChart2_ary,
+      colors: {
+        pattern: ['#585123', '#eec170', '#d36135', '#aca691'],
+      },
+    },
+  });
+};
 
 getOrderList();
